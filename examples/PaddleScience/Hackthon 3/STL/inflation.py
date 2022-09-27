7a86c74382cd6ebb7a674d879a50bd057f82cb0f
PuQing <me@puqing.work>
2022-09-27 11:11:42 +0800

    update 2d

--- examples/PaddleScience/Hackthon 3/STL/inflation.py
+++ examples/PaddleScience/Hackthon 3/STL/inflation.py
@@ -64,6 +64,89 @@ def inflation(mesh, dis):
     return new_mesh
 
 
+def offset(mesh, dis):
+    """
+    Offset the 2D mesh
+
+    Parameters
+    ----------
+    mesh : open3d.geometry.TriangleMesh
+        The mesh to be offset
+
+    dis : float
+        The distance to offset
+
+    Returns
+    -------
+    open3d.geometry.TriangleMesh
+    """
+
+    # check if the mesh is 2D
+    mesh.compute_triangle_normals()
+    normals = np.asarray(mesh.triangle_normals)
+    if not np.allclose(normals[:, :-1], 0):
+        raise ValueError("The mesh is not 2D")
+
+    mesh.remove_duplicated_vertices()
+    mesh.remove_degenerate_triangles()
+    mesh.remove_duplicated_triangles()
+    mesh.remove_unreferenced_vertices()
+    triangles = np.asarray(mesh.triangles)
+
+    edges = np.vstack(
+        [triangles[:, [0, 1]], triangles[:, [1, 2]], triangles[:, [2, 0]]]
+    )
+    edges = set(map(tuple, edges))
+    edges = np.array(list(edges))
+
+    vertices = np.asarray(mesh.vertices)[:, :-1]
+    edges_in_triangle = np.array(
+        [
+            np.intersect1d(
+                np.argwhere(triangles == edge[0])[:, 0],
+                np.argwhere(triangles == edge[1])[:, 0],
+            )
+            for edge in edges
+        ],
+        dtype=object,
+    )
+    surface_edges = edges[[len(i) == 1 for i in edges_in_triangle]]
+    edges_in_triangle = [i for i in edges_in_triangle if len(i) == 1]
+
+    edges_normals = []
+    for edge, triangle in zip(surface_edges, edges_in_triangle):
+        triangle = triangles[triangle].squeeze()
+        other_point = vertices[np.setdiff1d(triangle, edge)].squeeze()
+        edge = vertices[edge]
+        u = (other_point[0] - edge[0][0]) * (edge[0][0] - edge[1][0]) + (
+            other_point[1] - edge[0][1]
+        ) * (edge[0][1] - edge[1][1])
+        u = u / np.sum((edge[0] - edge[1]) ** 2)
+        edge_normal = edge[0] + u * (edge[0] - edge[1])
+        edge_normal = edge_normal - other_point
+        edges_normals.append(edge_normal)
+
+    edges_normals = np.array(edges_normals)
+    edges_normals = edges_normals / np.linalg.norm(edges_normals, axis=1)[:, None]
+
+    new_mesh = o3d.geometry.TriangleMesh()
+    new_vertices = []
+    for point in set(surface_edges.reshape(-1)):
+        index = np.argwhere(surface_edges == point)[:, 0]
+        normal = edges_normals[index]
+        d = np.ones(len(index)) * dis
+        new_point = np.linalg.lstsq(normal, d, rcond=None)[0]
+        new_point = vertices[point] + new_point
+        new_vertices.append(new_point)
+
+    new_vertices = np.hstack((np.array(new_vertices), np.zeros((len(new_vertices), 1))))
+    new_mesh.vertices = o3d.utility.Vector3dVector(new_vertices)
+    new_mesh.triangles = o3d.utility.Vector3iVector(triangles)
+    new_mesh.compute_triangle_normals()
+    new_mesh.compute_vertex_normals()
+    return new_mesh
+
+
 def sample_points(mesh, density=1, mode="uniform", seed=-1):
     """
     Sample points from the mesh
@@ -87,6 +170,7 @@ def sample_points(mesh, density=1, mode="uniform", seed=-1):
     -------
     open3d.geometry.PointCloud
     """
+
     total_points = int(mesh.get_surface_area() * density)
     if mode == "uniform":
         pc = mesh.sample_points_uniformly(total_points, seed=seed)
@@ -97,10 +181,89 @@ def sample_points(mesh, density=1, mode="uniform", seed=-1):
     return pc
 
 
+def edge_sample(mesh, density=1, mode="uniform", seed=-1):
+    """
+    Sample points from the 2D mesh edges
+
+    Parameters
+    ----------
+    mehs : open3d.geometry.TriangleMesh
+        The mesh to be sampled
+
+    density : int
+        The density of points to be sampled
+
+    mode : str
+        The mode of sampling, can be "uniform" or "grid"
+
+    seed : int
+        The seed of random number generator
+    """
+    mesh.remove_duplicated_vertices()
+    mesh.remove_degenerate_triangles()
+    mesh.remove_duplicated_triangles()
+    mesh.remove_unreferenced_vertices()
+    triangles = np.asarray(mesh.triangles)
+
+    edges = np.vstack(
+        [triangles[:, [0, 1]], triangles[:, [1, 2]], triangles[:, [2, 0]]]
+    )
+    edges = set(map(tuple, edges))
+    edges = np.array(list(edges))
+
+    vertices = np.asarray(mesh.vertices)
+    edges_in_triangle = np.array(
+        [
+            np.intersect1d(
+                np.argwhere(triangles == edge[0])[:, 0],
+                np.argwhere(triangles == edge[1])[:, 0],
+            )
+            for edge in edges
+        ],
+        dtype=object,
+    )
+    surface_edges = edges[[len(i) == 1 for i in edges_in_triangle]]
+    edges_points = vertices[surface_edges]
+    vlist = []
+    for edge in edges_points:
+        dx = edge[1][0] - edge[0][0]
+        dy = edge[1][1] - edge[0][1]
+        lenght = np.sqrt(dx**2 + dy**2)
+        if np.isclose(dx, 0):
+            y = sample(1, int(lenght * density), edge[0][1], edge[1][1], type=mode)
+            x = np.full_like(y, edge[0][0])
+        elif np.isclose(dy, 0):
+            x = sample(1, int(lenght * density), edge[0][0], edge[1][0], type=mode)
+            y = np.full_like(x, edge[0][1])
+        else:
+            k = dy / dx
+            b = edge[0][1] - k * edge[0][0]
+            x = sample(1, int(lenght * density), edge[0][0], edge[1][0], type=mode)
+            y = k * x + b
+        vlist.append(np.hstack([x, y, np.zeros_like(x)]))
+
+    vlist = np.vstack(vlist)
+    pc = o3d.geometry.PointCloud()
+    pc.points = o3d.utility.Vector3dVector(vlist)
+    return pc
+
+
+def sample(ndim, n, l_bound, u_bound, type="uniform"):
+    if l_bound > u_bound:
+        l_bound, u_bound = u_bound, l_bound
+    if type == "uniform":
+        return np.random.uniform(l_bound, u_bound, (n, ndim))
+    elif type == "grid":
+        return np.linspace(l_bound, u_bound, n).reshape(-1, 1)
+    else:
+        raise ValueError("type must be uniform or grid")
+
+
 def inflation_sample(
     mesh,
     dis=(float, list, np.ndarray),
     density=(float, list, np.ndarray),
+    dim=3,
     mode="uniform",
     seed=-1,
 ):
@@ -117,6 +280,9 @@ def inflation_sample(
     density : float or list or np.ndarray
         The density of points to be sampled
 
+    dim : int
+        The dimension of the mesh, can be 2 or 3
+
     mode : str
         The mode of sampling, can be "uniform" or "poisson_disk"
 
@@ -127,6 +293,7 @@ def inflation_sample(
     -------
     open3d.geometry.PointCloud
     """
+    assert dim in [2, 3], "The dimension of the mesh can only be 2 or 3"
     if isinstance(dis, (float, int)):
         dis = [dis]
     if isinstance(density, (float, int)):
@@ -136,8 +303,12 @@ def inflation_sample(
     new_mesh = mesh
     pcs = []
     for d, n in zip(dis, density):
-        new_mesh = inflation(new_mesh, d)
-        pc = sample_points(new_mesh, n, mode=mode, seed=seed)
+        if dim == 2:
+            new_mesh = offset(new_mesh, d)
+            pc = edge_sample(new_mesh, n, mode, seed)
+        else:
+            new_mesh = inflation(new_mesh, d)
+            pc = sample_points(new_mesh, n, mode=mode, seed=seed)
         pc.paint_uniform_color(np.random.random(3))
         pcs.append(pc)
     return pcs
